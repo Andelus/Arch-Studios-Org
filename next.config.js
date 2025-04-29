5d18e189e38102f733adc57b9e353fcef51f766d
“Andelus” <muhammadsuleiman284@gmail.com>
2025-04-28 18:55:03 -0700

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: '/api/webhooks/clerk',
+        destination: '/api/webhooks/clerk',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig; 
