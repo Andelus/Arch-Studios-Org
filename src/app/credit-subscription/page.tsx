15fbda46c2a73d1b6a67228830197ca695da5cdd
“Andelus” <muhammadsuleiman284@gmail.com>
2025-04-29 19:10:06 -0700

    modified:   src/app/credit-subscription/page.tsx

--- src/app/credit-subscription/page.tsx
+++ src/app/credit-subscription/page.tsx
@@ -1,8 +1,13 @@
+import { Suspense } from 'react';
 import { getSubscriptionPlans } from './SubscriptionPlans';
 import CreditSubscriptionClient from './CreditSubscriptionClient';
 
 export default async function CreditSubscription() {
   const plans = await getSubscriptionPlans();
   
-  return <CreditSubscriptionClient initialPlans={plans} />;
-} 
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CreditSubscriptionClient initialPlans={plans} />
+    </Suspense>
+  );
+}
