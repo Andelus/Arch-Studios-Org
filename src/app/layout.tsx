c8f91a5c9780c4bb8a996f15eafa9d919c4f69a6
“Andelus” <muhammadsuleiman284@gmail.com>
2025-05-22 05:06:13 -0700

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { GeistSans, GeistMono } from 'geist/font';
 import "./globals.css";
 import ThemeProvider from "../components/ThemeProvider";
-import { SupabaseAuthSync } from "../lib/auth-sync";
 import { DebugProvider } from "./debug-provider";
 
 export const metadata: Metadata = {
@@ -30,10 +29,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="w-full">
       <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased min-h-screen w-full flex flex-col`}>
-        <SupabaseAuthSync>
-          <ThemeProvider>{children}</ThemeProvider>
-          <DebugProvider />
-        </SupabaseAuthSync>
+        <ThemeProvider>{children}</ThemeProvider>
+        <DebugProvider />
       </body>
     </html>
   );
